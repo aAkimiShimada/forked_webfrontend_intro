2f49d5821bbcdd36f18d1b17d083a96cadccdee1
Akimi.Shimada <akimi.shimada@access-company.com>
2024-05-09 14:50:47 +0900

    課題9-2 終了

--- react/exercise/C09/Q2/index.tsx
+++ react/exercise/C09/Q2/index.tsx
@@ -17,7 +17,7 @@
 //
 // `action` は必要に応じて追加してください。
 //
-import { FC, useReducer, useState } from "react";
+import { FC, useReducer, useCallback } from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
 
@@ -29,9 +29,11 @@ declare type CarIndicatorProps = {
 
 const CarIndicator: FC<CarIndicatorProps> = (props) => {
   const { fulfilled, opened, launched } = props;
+  const carState = launched ? "carLaunched" : "carStopped";
+  const gateState = opened ? "gateOpened" : "gateClosed";
   return (
     <div className="carIndicator">
-      <div className={launched ? 'carLaunched' : 'carStopped'}>
+      <div className={carState}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="icon icon-tabler icon-tabler-car"
@@ -50,7 +52,7 @@ const CarIndicator: FC<CarIndicatorProps> = (props) => {
           <path d="M5 17h-2v-6l2 -5h9l4 5h1a2 2 0 0 1 2 2v4h-2m-4 0h-6m-6 -6h15m-6 0v-5" />
         </svg>
       </div>
-      <div className={opened ? 'gateOpened' : 'gateClosed'} />
+      <div className={gateState} />
     </div>
   );
 }
@@ -70,116 +72,145 @@ declare type ChildComponentState = {
 
 // 必要なActionは全部でいくつ？
 declare type ChildComponentActions = {
-  type: 'SET_FUEL_AMOUNT'; // これ以外にもActionのtypeは必要です。
+  type: ChildComponentActionType;
   payload?: number;
 }
 
-const SET_FUEL_AMOUNT = 'SET_FUEL_AMOUNT'; // これ以外にもActionのtypeは必要です。
+declare type ChildComponentActionType =
+  "SET_FUEL_AMOUNT" | "OPEN_GATE" |
+  "LAUNCH_CAR" | "RESET_STATE";
 
 // reducer の本体
 const reducer = (state: ChildComponentState, action: ChildComponentActions): ChildComponentState => {
   // 実装を追加しましょう
   switch(action.type) {
-    case SET_FUEL_AMOUNT: {
-      return state;
-    }
-    default:
-      return state;
+    case "SET_FUEL_AMOUNT":
+      const payload = action.payload!;
+
+      return {
+        fuelAmount: payload,
+        mode: payload == 100 ? CurrentMode.FULFILLED : CurrentMode.INITIAL
+      };
+    case "OPEN_GATE":
+      return {
+        fuelAmount: state.fuelAmount,
+        mode: CurrentMode.OPENED
+      };
+    case "LAUNCH_CAR":
+      return {
+        fuelAmount: state.fuelAmount,
+        mode: CurrentMode.LAUNCHED
+      };
+    case "RESET_STATE":
+      return initialState;
   }
 }
 
 // reducer 用の初期値を入れましょう
-const initialState = {};
+const initialState: ChildComponentState = {
+  fuelAmount: 0,
+  mode: CurrentMode.INITIAL
+};
+
+// UI ボタン生成の引数
+interface ButtonProps {
+  label: string,
+  action: ChildComponentActionType,
+  disabled: boolean
+}
 
 const ChildComponent: FC = () => {
+
   // useState が乱立しているので useReducer を用いてまとめましょう
-  // Range Control
-  const [fuelAmount, setFuelAmount] = useState<number>(0);
-  // State
-  const [fulfilled, setFulfilled] = useState<boolean>(false);
-  const [opened, setOpened] = useState<boolean>(false);
-  const [launched, setLaunched] = useState<boolean>(false);
-  
-  // Event Handler
-  // 各関数の実装を useReducer を用いるものに変更しましょう
-  const onChangeFuel = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // Input Range control
-    const amount = Number(e.target.value);
-    setFuelAmount(amount);
-    
-    // Set State
-    if (amount > 99) {
-      setFulfilled(true);
-    }
-  };
-  
-  const onClickLaunch = () => {
-    setLaunched(true);
-  };
-  
-  const onClickOpen = () => {
-    setOpened(true);
-  };
-  
-  const onClickReset = () => {
-    setFuelAmount(0);
-    setFulfilled(false);
-    setOpened(false);
-    setLaunched(false);
-  };
-  
-  // Disalbled flags
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Disabled flags
   // useReducer の state を活用するようにしましょう
-  const fuelRangeDisabled = fulfilled;
-  const openButtonDisalbled = !fuelRangeDisabled || opened;
-  const launchButtonDisabled = !fuelRangeDisabled || !openButtonDisalbled || launched;
-  const resetButtonDisabled = !fuelRangeDisabled;
-  
+  const fuelRangeDisabled =
+    state.mode != CurrentMode.INITIAL;
+  const openButtonDisabled =
+    state.mode != CurrentMode.FULFILLED;
+  const launchButtonDisabled =
+    state.mode != CurrentMode.OPENED;
+  const resetButtonDisabled =
+    state.mode == CurrentMode.INITIAL &&
+    state.fuelAmount == 0;
+
+  const isFulfilled = state.mode != CurrentMode.INITIAL;
+  const isOpened = state.mode == CurrentMode.OPENED || state.mode == CurrentMode.LAUNCHED;
+  const isLaunched = state.mode == CurrentMode.LAUNCHED;
+
+  function Button(props: ButtonProps) {
+
+    const cb_func = useCallback(
+      () => {
+        dispatch({ type: props.action })
+      },
+      [state]
+    );
+
+    return (
+      <button
+        type="button"
+        disabled={props.disabled}
+        onClick={cb_func}>{props.label}</button>
+    )
+
+  }
+
+  function Range(_props: {}) {
+
+    const cb_func = useCallback(
+      (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+          type: "SET_FUEL_AMOUNT",
+          payload: parseInt(e.target.value)
+        })
+      },
+      [state]
+    );
+
+    return <input
+      type="range"
+      id="fuel"
+      name="fuelTank"
+      min="0"
+      max="100"
+      step="1"
+      value={state.fuelAmount}
+      onChange={cb_func}
+      disabled={fuelRangeDisabled}
+    />;
+
+  }
+
   // Fuel Range Label
   // useReducer の state を活用するようにしましょう
-  const fuelLabelText = `Fuel: ${String(fuelAmount).padStart(3, '0')}%`;
+  const fuelLabelText = `Fuel: ${String(state.fuelAmount).padStart(3, '0')}%`;
 
   // Render
   // 変更内容を適用するようにしましょう
   return (
     <div className="subContainer">
       <CarIndicator
-        fulfilled={fulfilled}
-        opened={opened}
-        launched={launched}
+        fulfilled={isFulfilled}
+        opened={isOpened}
+        launched={isLaunched}
       />
-      <input
-         type="range"
-         id="fuel"
-         name="fuelTank"
-         min="0"
-         max="100"
-         step="1"
-         value={fuelAmount}
-         onChange={onChangeFuel}
-         disabled={fuelRangeDisabled}
-      />
-      <label htmlFor="fuel">
-        {fuelLabelText}
-      </label>
-      <button
-        onClick={onClickOpen}
-        disabled={openButtonDisalbled}
-      >
-        Open the gate
-      </button>
-      <button
-        onClick={onClickLaunch}
-        disabled={launchButtonDisabled}
-      >
-        Launch
-      </button>
-      <button
-        onClick={onClickReset}
-        disabled={resetButtonDisabled}
-      >
-        Reset
-      </button>
+      <Range />
+      <label htmlFor="fuel"> {fuelLabelText} </label>
+      <Button
+        label="Open the gate"
+        action="OPEN_GATE"
+        disabled={openButtonDisabled} />
+      <Button
+        label="Launch"
+        action="LAUNCH_CAR"
+        disabled={launchButtonDisabled} />
+      <Button
+        label="Reset"
+        action="RESET_STATE"
+        disabled={resetButtonDisabled} />
       <p>
         SVG Icons by Tabler Icons (https://tablericons.com/)
       </p>
