c812c62b26c6b0c7ece09bc2557a98d52f5b3391
Akimi.Shimada <akimi.shimada@access-company.com>
2024-05-10 13:50:01 +0900

    課題13-2 終了

--- react/exercise/C13/Q2/index.tsx
+++ react/exercise/C13/Q2/index.tsx
@@ -10,34 +10,48 @@ const INIT_COUNT = 10;
 
 function CountDown() {
   const [count, setCount] = useState(INIT_COUNT);
+  const [didRun, setDidRun] = useState(false);
 
   // TODO: useRef を使う (型は NodeJS.Timer | null)
+  type RefType = ReturnType<typeof setInterval> | null;
+  const timerRef = useRef<RefType>();
+
+  const countdown_func = () => {
+    setCount(count => count - 1);
+  };
 
   const start = () => {
     // TODO： setInterval を使う
     // @see https://developer.mozilla.org/ja/docs/Web/API/WindowOrWorkerGlobalScope/setInterval
+    setDidRun(true);
+    timerRef.current = setInterval(countdown_func,1000);
   };
 
   const stop = () => {
     // TODO： clearInterval を使う
     // @see https://developer.mozilla.org/ja/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval
+    clearInterval(timerRef.current!);
+    timerRef.current = null;
   };
+
   const reset = () => {
     stop();
-
+    setDidRun(false);
+    setCount(INIT_COUNT);
     // TODO: カウントをINIT_COUNTにリセット
   };
 
-  if (count === 0) {
-    stop();
-  }
+  if (count === 0) stop();
 
   return (
     <>
       <p>{count}</p>
       <p>
-        <button onClick={start}>Start</button>
-        <button onClick={reset}>Reset</button>
+        <button
+          disabled={didRun}
+          type="button" onClick={start}>Start</button>
+        <button
+          type="button" onClick={reset}>Reset</button>
       </p>
     </>
   );
